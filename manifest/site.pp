e74f6a5fba3c3ac00eed6a55fdbdab5fe05124d8
giftedwon <dougsegalowitz@gmail.com>
2018-04-01 10:46:59 +0300

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,9 +1,12 @@
 node 'master.puppet.vm' {
+  include dockeragent
   include role::master_server
 }
 node /^web/ { 
+  include dockeragent
   include role::app_server
 }
 node /^db/ {
+  include dockeragent
   include role::db_server
 }
