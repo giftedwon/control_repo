7c03d1fd3c6764d2d7b3193bb99e3c8ad13c82a8
giftedwon <dougsegalowitz@gmail.com>
2018-04-02 14:54:48 +0300

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -7,7 +7,6 @@ docker::image { 'ubuntu':
 
 docker::run { 'helloworld':
   image   => 'ubuntu',
-  command => '/bin/sh -c "while true; do echo hello world; sleep 1; done"',
   hostname => 'hello.puppet.vm',  
 }
 
